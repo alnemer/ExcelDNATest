533b47013962bcd30ef91847f77d4d30d671e1ff
Masayuki Takeda <mtakeda.enigsol@gmail.com>
2014-02-21 17:34:11 +0900

    Initial commit

---
+++ ExcelDNATest/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelDNATest
+{
+    public class Class1
+    {
+    }
+}
